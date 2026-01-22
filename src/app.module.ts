d77006ff76044c467e2ccb21a3854c30dc947aa5
anishsharma271 <anish.sharmas271@gmail.com>
2026-01-22 15:03:22 +0530

    done swagger and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -11,10 +13,37 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+      TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    url: configService.get<string>('DATABASE_URL'),
+    autoLoadEntities: true,
+    synchronize: true,
+    logging: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+}),
     FirebaseModule,
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private dataSource: DataSource) { }
+
+  onModuleInit() {
+    if (this.dataSource.isInitialized) {
+      console.log('✅ Database connected successfully');
+    } else {
+      console.error('❌ Failed to connect to the database');
+    }
+  }
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer.apply(LoggerMiddleware).forRoutes('*');
+  // }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,66 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './common/utils/interceptor/response.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:5173',
+  'http://localhost:8080'
+];
+
   // Enable CORS for frontend integration
-  app.enableCors();
-  
+app.enableCors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`❌ CORS error: Origin ${origin} not allowed by CORS`), false);
+    }
+  },
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
   // Enable validation pipe globally
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+        forbidNonWhitelisted: true,
       transform: true,
     }),
   );
-  
+     app.useGlobalInterceptors(new ResponseInterceptor());
+
+     // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription('E-Commerce API with Authentication and RBAC')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`🚀 Ecommerce API is running on: http://localhost:${process.env.PORT ?? 3000}`);
+   console.log(`📚 Swagger documentation available at: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
